867103085faefe39f99efa3dca2b9d50a8f50bb8
Aman Thakur <builtbyaman@gmail.com>
2022-11-26 21:45:57 +0530

    MVC

---
+++ src/js/helpers.js
@@ -0,0 +1,20 @@
+import { TIMEOUT_SECS } from './config';
+
+export async function getJSON(url) {
+  try {
+    const res = await Promise.race([(fetch(url), timeout(TIMEOUT_SECS))]);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export function timeout(s) {
+  return new Promise((_, reject) => {
+    setTimeout(function () {
+      reject(new Error(`Request took too long. Timeout after ${s} seconds`));
+    }, s * 1000);
+  });
+}
---
+++ src/js/model.js
@@ -0,0 +1,25 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
+export const state = {
+  recipe: {},
+};
+
+export async function loadRecipe(id) {
+  try {
+    const data = await getJSON(`${API_URL}/${id}?key=${process.env.API_KEY}`);
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    console.error(`💥`, err);
+  }
+}
